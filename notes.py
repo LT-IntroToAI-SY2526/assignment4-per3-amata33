008d0298e8ccfbbaeffa18995baa28183f687697
Aaron Mata <amata24@cps.edu>
2025-10-23 10:22:58 -0500

    Dog class notes

--- notes.py
+++ notes.py
@@ -2,3 +2,29 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog:
+    """A simple Dog class to learn OOP
+    
+    Attributes:
+    fur_col - color of the dog's fur
+    name - name of the dog
+    age - age of the dog
+    fav_food - favorite food"""
+
+    def __init__ (self, furcolor, name, age, fav_food):
+        """initialize new zawg"""
+        #python: instance var are done inside the constructor
+        self.furcolor = furcolor
+        self.name = name
+        self.age = age
+        self.fav_food = fav_food
+    
+    myZawg = Dog("black and white", "hacker", 2, "tortillas")
+
+    def __str__(self):
+        """return a string representation of a dog"""
+        return f"{self.name} is a {self.age} year old {self.furcolor} who loves {self.fav_food}"
+
+    def bark(self):
+        #make the dog bark
+        return f"{self.name} says guau guau!!!"
